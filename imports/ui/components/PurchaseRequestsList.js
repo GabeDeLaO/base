fd690da2828e459d5026869559dcf1716df60166
Gabriel De La O <matador.gabe@gmail.com>
2017-01-16 19:21:46 -0600

    saving prog.

--- imports/ui/components/PurchaseRequestsList.js
+++ imports/ui/components/PurchaseRequestsList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { ListGroup, ListGroupItem, Alert } from 'react-bootstrap';
+import { ListGroup, ListGroupItem, Alert, Button, Row, Col, Glyphicon } from 'react-bootstrap';
 
 export default class PurchaseRequestsList extends React.Component {
 
@@ -17,9 +17,22 @@ export default class PurchaseRequestsList extends React.Component {
 				{ this.props.purchases.length > 0 ? 
 					<ListGroup className="RequestsList">
 						{this.props.purchases.map( (purchase) => 
+							
 							<ListGroupItem key={ purchase._id }>
-								<strong>{purchase.user_name }</strong> asked for <strong>{purchase.qty}</strong> items @ <strong>${purchase.dollarAmount}</strong> each Request ID: <code>{purchase._id}</code>
-							</ListGroupItem> )}
+								<Row>
+									<Col xs={7}>
+										<strong>{purchase.user_name }</strong> asked for <strong>{purchase.qty}</strong> items @ <strong>${purchase.dollarAmount}</strong> each<br/>
+										 Request ID: <code>{purchase._id}</code>
+									</Col>
+									<Col xs={5}>
+										<Button className="btn-info btn-sm pull-right"><a href={purchase.link} target="blank">Link</a></Button>
+										<Button className="btn-white btn-sm pull-right">Complete</Button>
+										<Button className="btn-white btn-sm pull-right"><Glyphicon glyph="star" />Deny</Button>
+									</Col>
+								</Row>
+							</ListGroupItem> 
+
+						)}
 					</ListGroup>
 					: 
 					<Alert bsStyle="warning">No requests yet.</Alert>
